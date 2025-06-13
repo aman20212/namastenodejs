cd97e8537e69730a2b4cd339fc09d8963a85da6f
Aman Singh <amansinghsengar01@gmail.com>
2025-06-13 07:00:30 +0530

    Playing with route params and query params in route handlers or controller

--- src/app.js
+++ src/app.js
@@ -1,25 +1,27 @@
 const express = require('express');
 const app = express();
 
-app.use('/user', (req, res) => {
-    res.send('Hello from user')
-})
+// app.use('/user', (req, res) => {
+//     res.send('Hello from user')
+// })
 
-app.get('/user', (req, res) => {
+app.get('/user/:userId/:name/:password', (req, res) => {
+    console.log('Request params::', req.params);
+    console.log('Request Query Paramaters', req.query);
     res.send({ firstName: 'Aman', lastName: 'Singh' })
 })
 
-app.post('/user', (req, res) => {
-    res.send('User data saved successfully in the DB')
-})
+// app.post('/user', (req, res) => {
+//     res.send('User data saved successfully in the DB')
+// })
 
-app.delete('/user', (req, res) => {
-    res.send('User Deleted from DB');
-})
+// app.delete('/user', (req, res) => {
+//     res.send('User Deleted from DB');
+// })
 
-app.use('/', (req, res) => {
-    res.send('Hello from express server');
-})
+// app.use('/', (req, res) => {
+//     res.send('Hello from express server');
+// })
 
 app.listen(3000, () => {
     console.log('Server is up and running on port 3000....');
