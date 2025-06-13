20a03c5c8484885e231e3026b2ed66bd9de5b412
Aman Singh <amansinghsengar01@gmail.com>
2025-06-13 06:34:39 +0530

    Handled GET, POST request

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const app = express();
 
+app.use('/user', (req, res) => {
+    res.send('Hello from user')
+})
+
+app.get('/user', (req, res) => {
+    res.send({ firstName: 'Aman', lastName: 'Singh' })
+})
+
+app.post('/user', (req, res) => {
+    res.send('User data saved successfully in the DB')
+})
+
+app.delete('/user', (req, res) => {
+    res.send('User Deleted from DB');
+})
+
 app.use('/', (req, res) => {
     res.send('Hello from express server');
 })
