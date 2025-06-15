ad0badf599da6e1500153d8ee83d30741d482f6f
Aman Singh <amansinghsengar01@gmail.com>
2025-06-15 07:52:18 +0530

    S2Episode07 Diving Into APIs

--- src/app.js
+++ src/app.js
@@ -18,6 +18,55 @@ app.post("/signup", async (req, res) => {
     }
 });
 
+app.get('/user', async (req, res) => {
+    try {
+        const userEmail = req.body.emailId;
+        console.log(userEmail);
+        const user = await User.findOne({ emailId: userEmail });
+        if (!user) {
+            res.status(404).send("User not found");
+        } else {
+            res.send(user);
+        }
+    } catch (error) {
+        res.status(400).send("Something went wrong ");
+    }
+});
+
+app.get('/feed', async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.send(users);
+    } catch (error) {
+        res.status(400).send("Something went wrong ");
+    }
+
+});
+
+app.delete('/user', async (req, res) => {
+    const userId = req.body.userId;
+    try {
+        const user = await User.findByIdAndDelete({ _id: userId });
+        res.send('User Deleted Successfully');
+    } catch (error) {
+        res.status(400).send('User Deletion Failed');
+    }
+});
+
+app.patch("/user", async (req, res) => {
+    const userId = req.body.userId;
+    const data = req.body;
+    try {
+        const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+            returnDocument: "after",
+        });
+        console.log(user);
+        res.send("User updated successfully");
+    } catch (err) {
+        res.status(400).send("Something went wrong ");
+    }
+});
+
 
 connectDB().then(() => {
     console.log('DB Connection was successful');
