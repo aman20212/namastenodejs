a454972227f78e3657342fadb191ebca7c4c122e
Aman Singh <amansinghsengar01@gmail.com>
2025-06-17 16:08:56 +0530

    S2E9 Encrypting password using bcrypt npm package

--- src/app.js
+++ src/app.js
@@ -3,18 +3,49 @@ const app = express();
 const connectDB = require("./config/database");
 const User = require("./models/users");
 
-const { adminAuth, userAuth } = require('./middlewares/auth');
+const { validateSignUpData } = require("./utils/validation");
+const bcrypt = require("bcrypt");
 
 app.use(express.json());
 
+app.post("/login", async (req, res) => {
+    try {
+        const { emailId, password } = req.body;
+
+        const user = await User.findOne({ emailId: emailId });
+        if (!user) {
+            throw new Error("Invalid credentials");
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (isPasswordValid) {
+            res.send("Login Successful!!!");
+        } else {
+            throw new Error("Invalid credentials");
+        }
+    } catch (err) {
+        res.status(400).send("ERROR : " + err.message);
+    }
+});
+
 app.post("/signup", async (req, res) => {
     // Creating a new instance of the User model
-    const user = new User(req.body);
+
     try {
+        validateSignUpData(req);
+        const { firstName, lastName, emailId, password } = req.body;
+        const passwordHash = await bcrypt.hash(password, 10);
+        console.log(passwordHash);
+        const user = new User({
+            firstName,
+            lastName,
+            emailId,
+            password: passwordHash,
+        });
         await user.save();
         res.send("User Added successfully!");
     } catch (err) {
-        res.status(400).send("Error saving the user:" + err.message);
+        res.status(400).send("ERROR : " + err.message);
     }
 });
 
