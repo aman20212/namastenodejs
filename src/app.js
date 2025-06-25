1226a63bed537b25e9795893efe3585e1bf9dc34
Aman Singh <amansinghsengar01@gmail.com>
2025-06-25 07:33:28 +0530

    S2E11 Diving into the APIs and express Router

--- src/app.js
+++ src/app.js
@@ -2,81 +2,19 @@ const express = require('express');
 const app = express();
 const connectDB = require("./config/database");
 const User = require("./models/users");
-
-const { validateSignUpData } = require("./utils/validation");
-const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser");
-const jwt = require("jsonwebtoken");
-const { userAuth } = require("./middlewares/auth");
+
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.post("/login", async (req, res) => {
-    try {
-        const { emailId, password } = req.body;
+const authRouter = require("./routes/auth");
+const profileRouter = require("./routes/profile");
+const requestRouter = require("./routes/request");
 
-        const user = await User.findOne({ emailId: emailId });
-        if (!user) {
-            throw new Error("Invalid credentials");
-        }
-        const isPasswordValid = await user.validatePassword(password);
-
-        if (isPasswordValid) {
-            // Create a JWT Token
-
-            const token = await user.getJWT();
-
-            // Add the token to cookie and send the response back to the user
-            res.cookie("token", token, {
-                expires: new Date(Date.now() + 8 * 3600000),
-            });
-
-            res.send("Login Successful!!!");
-        } else {
-            throw new Error("Invalid credentials");
-        }
-    } catch (err) {
-        res.status(400).send("ERROR : " + err.message);
-    }
-});
-
-app.post("/signup", async (req, res) => {
-    // Creating a new instance of the User model
-
-    try {
-        validateSignUpData(req);
-        const { firstName, lastName, emailId, password } = req.body;
-        const passwordHash = await bcrypt.hash(password, 10);
-        console.log(passwordHash);
-        const user = new User({
-            firstName,
-            lastName,
-            emailId,
-            password: passwordHash,
-        });
-        await user.save();
-        res.send("User Added successfully!");
-    } catch (err) {
-        res.status(400).send("ERROR : " + err.message);
-    }
-});
-
-app.get("/profile", userAuth, async (req, res) => {
-    try {
-        const user = req.user;
-        res.send(user);
-    } catch (err) {
-        res.status(400).send("ERROR : " + err.message);
-    }
-});
-
-app.post("/sendConnectionRequest", userAuth, async (req, res) => {
-    const user = req.user;
-    // Sending a connection request
-    console.log("Sending a connection request");
-    res.send(user.firstName + "sent the connect request!");
-});
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/", requestRouter);
 
 
 
